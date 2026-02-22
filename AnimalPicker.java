ae2ff4f13e53064803c97a5606c2400e6d9d1dc8
e-j-l <emmettjleary@gmail.com>
2026-02-22 12:53:51 -0500

    infinitely loops until user inputs a valid response

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -7,23 +7,32 @@ import java.util.Scanner;
 public class AnimalPicker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        // craetes loop to continuously ask for inputs until a valid one is detected
+        boolean valid = false;
 
-        // ask user which animal they want to see
-        System.out.println("Which animal would you like to see: a dog or a cat?");
-        String animal = scanner.nextLine();
+        while(!valid) {
 
-        // check which animal and prints image statement
-        if(animal.equals("dog")) {
-            System.out.println("DOG ASCII ART HERE");
+            // ask user which animal they want to see
+            System.out.println("Which animal would you like to see: a dog or a cat?");
+            String animal = scanner.nextLine();
 
-        } else if(animal.equals("cat")) {
-            System.out.println("CAT ASCII ART HERE");
+            // check which animal and prints image statement
+            if(animal.equals("dog")) {
+                valid = true;
+                System.out.println("DOG ASCII ART HERE");
 
-        } else {
-            System.out.println("Error: Unknown Species");
-            
+            } else if(animal.equals("cat")) {
+                valid = true;
+             System.out.println("CAT ASCII ART HERE");
+
+            } else {
+                System.out.println("Error: Unknown animal, please try again");
+
+            }
+
         }
-
         scanner.close();
+
     }
 }
